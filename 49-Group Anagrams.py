2f32ebb58efa888340b015be010c95f7467a55c5
ferdinandnfriasII <89413337+ferdinandnfriasII@users.noreply.github.com>
2022-05-20 11:01:14 -0500

    Create 49-Group Anagrams.py
    use case for collections.defaultdict, and "".join() function.

---
+++ 49-Group Anagrams.py
@@ -0,0 +1,39 @@
+#leetcode practice question 49
+#given an array of strings, group the anagrams together.
+
+import collections
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        strs = ["eat","tea","tan","ate","nat","bat"]
+    
+        eat -> aet
+        tea -> aet 
+        
+        Note: when sorted, therefore if sorted str are equal they belong in the same list
+        
+        so create list dictionary for each possible sorted string, if the sorted string exists
+        in the dictionary then append that string to the list. 
+        
+
+        ex. 
+                       0               1                  2
+        charDict = { "aet" : ["eat", "tea", ], "ant" : ["tan", ] }
+
+        strs = ["eat","tea","tan","ate","nat","bat"]
+                  0
+                        1
+                            2....
+        """
+        
+        charDict = collections.defaultdict(list)
+        
+        print(charDict)
+        
+        for s in strs:
+            charDict["".join(sorted(s))].append(s)
+        
+            
+        return charDict.values()
+            
